ed325986cd91af3fded86624d7f39043abd14ee8
Kairav <kairavdutta@gmail.com>
2026-05-19 00:05:24 +0530

    initialised mediapipe and cam

--- sensing.py
+++ sensing.py
@@ -8,7 +8,16 @@ from utils.moves import Moves
 
 class Sensing:
     def __init__(self) -> None:
-        pass
+        # Initialize MediaPipe
+        mp_hands = mp.solutions.hands  # type: ignore
+        draw = mp.solutions.drawing_utils  # type: ignore
+        hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.9, min_tracking_confidence=0.9)
+        # Open webcam
+        cap = cv2.VideoCapture(0)
+        # Variables to track motion for horizontal slap / turn detection
+        prev_x = None
+        prev_time = None
+        slap_cooldown = 0
     def fingers_up(self, hand_landmarks) -> list[int] | None: #Can bitmask if bhaiya asks for optimization
         pass
     def thumb_pointing_down(self, hand_landmarks) -> bool:
